7f720f85eec160a23305e875b0bf19ef866b8b08
MAGANER <maganer.krik@mail.ru>
2024-02-01 02:14:23 +0300

    load and draw image

--- main.c
+++ main.c
@@ -8,8 +8,10 @@ int init_window(SDL_Window** window,
 		SDL_Renderer** renderer,
 		int width, int height);
 SDL_Surface* load_image_surface(char* path);
+SDL_Texture* create_texture(SDL_Surface** image_surface,SDL_Renderer** renderer);
 int main(int argc, char** argv)
 {
+  //#1: check args
   if(argc != 2)
     {
       printf("%s\n","cropper error: incorrect number of arguments");
@@ -17,15 +19,21 @@ int main(int argc, char** argv)
       return 1;
     }
 
+  //#2: load image
   SDL_Surface* image_surface = load_image_surface(argv[1]);
   if(image_surface == NULL) return 1;
   
-  
+
+  //#3: prepare window and renderer
   SDL_Window* window     = NULL;
   SDL_Renderer* renderer = NULL;
-  
   if(init_window(&window,&renderer, image_surface->w,image_surface->h) != 0) return 1;
 
+  //#4: create drawable texture based on image surface
+  SDL_Texture* texture = create_texture(&image_surface, &renderer);
+  if(texture == NULL) return 1;
+
+  
   SDL_Event e;
   bool quit = false;
   while(!quit)
@@ -35,10 +43,12 @@ int main(int argc, char** argv)
 	if( e.type == SDL_QUIT ) quit = true;
       }
 
-    SDL_RenderClear(renderer);                        
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
     SDL_RenderPresent(renderer);    
   }
-  
+
+  SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
   return 0;
@@ -88,8 +98,21 @@ SDL_Surface* load_image_surface(char* path)
     SDL_Surface* surface = IMG_Load(path);
     if(surface == NULL )
     {
-        printf( "cropper error:: %s\n", path, IMG_GetError() );
+        printf( "cropper error: %s\n", path, IMG_GetError() );
 	return NULL;
     }
     return surface;
+}
+SDL_Texture* create_texture(SDL_Surface** image_surface, SDL_Renderer** renderer)
+{
+  //i don't check is image_surface NULL, because it can be NULL. it's already checked
+  
+  SDL_Texture* texture = SDL_CreateTextureFromSurface(*renderer,*image_surface);
+  if(texture == NULL)
+    {
+      printf("cropper error: %s\n",SDL_GetError());
+      return NULL;
+    }
+  SDL_FreeSurface(*image_surface);
+  return texture;
 }
