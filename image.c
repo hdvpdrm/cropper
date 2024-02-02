1a9d73e46abfbe4bf8f77524df8276b7b432fb8a
MAGANER <maganer.krik@mail.ru>
2024-02-02 17:29:58 +0300

    saving function has additional parameter - output type

--- image.c
+++ image.c
@@ -4,7 +4,6 @@ SDL_Surface* load_image_surface(char* path)
 {  
   if(path == NULL) return NULL;
   
-
     //Load image at specified path
     SDL_Surface* surface = IMG_Load(path);
     if(surface == NULL )
@@ -27,7 +26,11 @@ SDL_Texture* create_texture(SDL_Surface** image_surface, SDL_Renderer** renderer
   SDL_FreeSurface(*image_surface);
   return texture;
 }
-int save_image(const char* file_name, SDL_Renderer** renderer,SDL_Texture** texture, SDL_Rect* area)
+int save_image(const char* file_name,
+	       SDL_Renderer** renderer,
+	       SDL_Texture** texture,
+	       SDL_Rect* area,
+	       int ext_type)
 {
   SDL_Texture* target = SDL_GetRenderTarget(*renderer);
   SDL_SetRenderTarget(*renderer, *texture);
@@ -37,7 +40,10 @@ int save_image(const char* file_name, SDL_Renderer** renderer,SDL_Texture** text
   SDL_Surface* surface = SDL_CreateRGBSurface(0, width, height, 32, 0, 0, 0, 0);
 
   SDL_RenderReadPixels(*renderer, area, surface->format->format, surface->pixels, surface->pitch);
-  IMG_SavePNG(surface, file_name);
+
+  if(ext_type == 0) IMG_SavePNG(surface, file_name);
+  else IMG_SaveJPG(surface,file_name,100);
+  
   SDL_FreeSurface(surface);
   SDL_SetRenderTarget(*renderer, target);
 }
--- image.h
+++ image.h
@@ -7,5 +7,9 @@
 
 extern SDL_Surface* load_image_surface(char* path);
 extern SDL_Texture* create_texture(SDL_Surface** image_surface,SDL_Renderer** renderer);
-extern int save_image(const char* file_name, SDL_Renderer** renderer,SDL_Texture** texture, SDL_Rect* area);
+extern int save_image(const char* file_name,
+		      SDL_Renderer** renderer,
+		      SDL_Texture** texture,
+		      SDL_Rect* area,
+		      int ext_type);
 #endif
